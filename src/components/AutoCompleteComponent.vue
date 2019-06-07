fe29f32bf8f88c0b378d09d9710cea4a3eeeb3e3
Carlos Gabriel <gabrielggff25@gmail.com>
2019-06-07 17:39:04 -0300

    AutoComplete, improved RegisterContact

---
+++ src/components/AutoCompleteComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="autoCompleteComponent">
+    <ion-list>
+      <ion-item>
+        <ion-label position="floating">Aplicações</ion-label>
+        <ion-select @ionChange="onDigitAutoComplete" multiple="true">
+          <ion-select-option v-for="item in list" :key="item" :value="item">{{ item }}</ion-select-option>
+        </ion-select>
+      </ion-item>
+    </ion-list>
+  </div>
+</template>
+<script>
+import { isBlank } from "voca";
+export default {
+  name: "autoCompleteComponent",
+  props: {
+    list: Array
+  },
+  methods: {
+    onDigitAutoComplete(option) {
+      let {
+        detail: { value }
+      } = option;
+      if (isBlank(value)) return;
+      this.$emit("selecteds", value);
+    }
+  }
+};
+</script>
+<style>
+</style>
--- src/views/RegisterContact.vue
+++ src/views/RegisterContact.vue
@@ -1,18 +1,55 @@
 <template>
-  <div id="registerContact">
+  <ion-app>
     <NavBarComponent :name="'Register Contact'"></NavBarComponent>
-    <ion-content>
+    <ion-content padding>
+      <form @submit="checkForm" action="https://vuejs.org/" method="post">
+        <ion-item>
+          <ion-label position="floating">Nome</ion-label>
+          <ion-input autofocus placeholder="Digite o nome do contato" type="text" required></ion-input>
+        </ion-item>
+        <ion-item>
+          <ion-label position="floating">Telefone</ion-label>
+          <ion-input placeholder="Digite o seu telefone" type="number" required></ion-input>
+        </ion-item>
+        <ion-item>
+          <ion-label position="floating">Email</ion-label>
+          <ion-input placeholder="Digite o seu email" type="email" required></ion-input>
+        </ion-item>
+        <AutoCompleteComponent :list="applications" :selecteds="onSelecteds"></AutoCompleteComponent>
+        <ion-item lines="none">
+          <ion-button type="submit" size="default" color="primary">Add</ion-button>
+          <ion-button size="default" slot="end" color="danger">Cancel</ion-button>
+        </ion-item>
+      </form>
     </ion-content>
-  </div>
-</template>
+  </ion-app>
+</template> 
 
 <script>
 import NavBarComponent from "../components/NavBarComponent";
+import AutoCompleteComponent from "../components/AutoCompleteComponent";
+
 export default {
   name: "registerContact",
+  data() {
+    return {
+      applications: [
+        "GPV Pelotização",
+        "GPV Portos",
+        "GPV Minas",
+        "GPV Ferrovias"
+      ],
+      form: Object
+    };
+  },
   components: {
-    NavBarComponent
-  }
+    NavBarComponent,
+    AutoCompleteComponent
+  },
+  methods: {
+    onSelecteds() {}
+  },
+  mounted() {}
 };
 </script>
 
